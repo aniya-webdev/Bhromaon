33b6212e15e3536cf3cb9d4dba736660ff0ea706
aniya-webdev <aniya-webdev@yandex.ru>
2023-01-14 14:31:02 +0300

    create

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav');
+
+navIcon.addEventListener('click', function () {
+	this.classList.toggle('nav-icon--active');
+	nav.classList.toggle('nav--active');
+});
+
+const navLinks = document.querySelectorAll('.nav a');
+
+navLinks.forEach(function (item) {
+	item.addEventListener('click', function () {
+		navIcon.classList.remove('nav-icon--active');
+		nav.classList.remove('nav--active'); 
+	});
+});
+
+//# sourceMappingURL=main.js.map
